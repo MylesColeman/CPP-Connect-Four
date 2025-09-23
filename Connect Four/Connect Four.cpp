e1b8f79b28e5369e64d6565189ad3d422d082e9b
E4109732 <E4109732@TU115069>
2025-09-23 11:49:36 +0100

    got the board displaying

--- Connect Four/Connect Four.cpp
+++ Connect Four/Connect Four.cpp
@@ -2,8 +2,40 @@
 //
 
 #include <iostream>
+#include <vector>
+
+const int rows = 6;
+const int cols = 7;
+
+const char empty = '-';
+const char divider = '|';
+const char nought = 'O';
+const char cross = 'X';
+
+void printBoard(const std::vector<std::vector<char>>& board)
+{
+    for (int r = 0; r < rows; r++)
+    {
+		std::cout << divider;
+
+        for (int c = 0; c < cols; c++)
+        {
+            std::cout << " " << board[r][c] << " " << divider;
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main()
 {
+    std::cout << "Connect Four!" << std::endl;
+    std::cout << std::endl;
+
+	std::vector<std::vector<char>> board(rows, std::vector<char>(cols, empty));
     
+	printBoard(board);
+    std::cout << std::endl;
+
+	std::cout << "Noughts goes first, which row do you want to drop your counter in? (1-7)" << std::endl;
+    //std::cin >> 
 }
