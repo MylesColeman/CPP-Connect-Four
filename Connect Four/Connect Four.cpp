0750aaea4991033dc3868384efae0e6adc166818
E4109732 <E4109732@TU115069>
2025-09-23 12:09:10 +0100

    can place noughts and crosses, no win detection. no incorredct column detection

--- Connect Four/Connect Four.cpp
+++ Connect Four/Connect Four.cpp
@@ -12,6 +12,7 @@ const char divider = '|';
 const char nought = 'O';
 const char cross = 'X';
 
+// Prints the board to the console
 void printBoard(const std::vector<std::vector<char>>& board)
 {
     for (int r = 0; r < rows; r++)
@@ -36,6 +37,44 @@ int main()
 	printBoard(board);
     std::cout << std::endl;
 
-	std::cout << "Noughts goes first, which row do you want to drop your counter in? (1-7)" << std::endl;
-    //std::cin >> 
+    int chosenColumn = 0;
+	
+    const bool gameOver = false;
+
+    while (gameOver == false) // Loops whilst game isn't over
+    {
+        std::cout << "Nought's turn, which row do you want to drop your counter in? (1-7)" << std::endl;
+        std::cout << std::endl;
+        std::cin >> chosenColumn;
+        chosenColumn -= 1; // Adjust for 0 index
+
+        for (int r = rows - 1; r >= 0; r--)
+        {
+            if (board[r][chosenColumn] == empty)
+            {
+                board[r][chosenColumn] = nought;
+                break;
+            }
+        }
+
+        printBoard(board);
+        std::cout << std::endl;
+
+        std::cout << "Cross's turn, which row do you want to drop your counter in? (1-7)" << std::endl;
+        std::cout << std::endl;
+        std::cin >> chosenColumn;
+        chosenColumn -= 1; // Adjust for 0 index
+
+        for (int r = rows - 1; r >= 0; r--)
+        {
+            if (board[r][chosenColumn] == empty)
+            {
+                board[r][chosenColumn] = cross;
+                break;
+            }
+        }
+
+        printBoard(board);
+        std::cout << std::endl;
+    }
 }
